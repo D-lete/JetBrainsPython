042b69e341c4cd34fbfca5b72995883f120670f0
Benjamin A Pillmore <bapillmore@users.noreply.github.com>
2020-08-14 18:29:27 +0200

    rewrite

--- Coffee Machine.py
+++ Coffee Machine.py
@@ -1,33 +1,34 @@
-water_needed = 200
-milk_needed = 50
-coffee_needed = 15
+water_needed = int(200)
+milk_needed = int(50)
+coffee_needed = int(15)
 
 cups_of_coffee = int(input("How many cups of coffee would you like?: "))
 
-water_reserve = int(input(400))
-milk_reserve = int(input(100))
-coffee_reserve = int(input(30))
+water_reserve = int(400)
+milk_reserve = int(100)
+coffee_reserve = int(30)
 
 
 def coffee_water():
 	cw = cups_of_coffee * water_needed
-	return int()
+	return cw
 
 
 def coffee_milk():
 	cm = cups_of_coffee * milk_needed
-	return int()
+	return cm
 
 
 def coffee_coffee():
 	cc = cups_of_coffee * coffee_needed
-	return int()
+	return cc
 
 
 def coffee_product():
-	min(water_reserve // coffee_water(), milk_reserve // coffee_milk(), coffee_reserve // coffee_coffee())
-	return int()
-	
+	cups = min(water_reserve // coffee_water(), milk_reserve // coffee_milk(),
+												coffee_reserve // coffee_coffee())
+	return cups
+
 
 if cups_of_coffee > 0 and water_reserve > 0 and milk_reserve > 0 and coffee_reserve > 0:
 	print(f"{coffee_product()} cups of coffee can be made.")
