4694f589e14caebaa634e4ad3579b365206f12b4
Benjamin A Pillmore <41969676+bapillmore@users.noreply.github.com>
2021-03-04 12:23:01 +0100

    Initial

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -5,49 +5,57 @@ import random
 
 class ATM:
 
-    def __init__(self):
-        options_root = {1: ATM.__generator__(self),
-                        2: ATM.__login__(self),
-                        0: ATM.__exit__(self)}
+    def __init__(self, card_number, pin_code, balance):
+        self.card_number = card_number
+        self.pin_code = pin_code
+        self.balance = balance
+        options_root = {1: ATM.generator(self, card_number, pin_code, balance),
+                        2: ATM.login(self),
+                        0: ATM.exit(self)}
+
         print('1. Create an account\n2. Log into account\n0. Exit')
         user_input = int(input('> '))
         options_root.get(user_input)
 
-    def __generator__(self):
-        self.card_number = str('400000') + str(random.)
-        self.pin_code = str(random.randrange(1000, 9999, 1))
-        self.balance = 0
-        print('Your card number:\n{}\nYour card PIN:\n{}')
-        ATM.__init__(self)
+    def generator(self, card_number, pin_code, balance):
+        if card_number == 0:
+            self.card_number = str('400000') + str(random.randint(1000000000, 9999999999))
+        if pin_code == 0:
+            self.pin_code = str(random.randrange(1000, 9999, 1))
+        if balance == 0:
+            self.balance = 0
+        print('Your card number:\n %s Your card PIN:\n %s' % (self.card_number, self.pin_code))
+        ATM.__init__(self, card_number=self.card_number, pin_code=self.pin_code, balance=self.balance)
 
-    def __login__(self):
+    def login(self):
         card = input('Enter your card number: \n> ')
         pin = input('Enter your PIN: \n> ')
         if card == self.card_number and pin == self.pin_code:
             print('You have successfully logged in!')
-            ATM.__acct__(self)
+            ATM.acct(self)
         elif card != self.card_number or pin != self.pin_code:
             print('Wrong card number or PIN!')
-            ATM.__init__(self)
+            ATM.__init__(self, card_number=self.card_number, pin_code=self.pin_code, balance=self.balance)
 
-    def __acct__(self):
-        options_acct = {1: ATM.__balance__(self),
-                        2: ATM.__logout__(self),
-                        0: ATM.__exit__(self)}
+    def acct(self):
+        options_acct = {1: ATM.balance(self),
+                        2: ATM.logout(self),
+                        0: ATM.exit(self)}
         print('1. Balance\n2. Log into account\n0. Exit')
         user_input = int(input('> '))
         options_acct.get(user_input)
 
-    def __balance__(self):
+    def balance(self):
         print('Balance: ' + str(self.balance))
-        ATM.__acct__(self)
+        ATM.acct(self)
 
-    def __logout__(self):
+    def logout(self):
         print('You have been successfully logged out!')
-        ATM.__init__(self)
+        ATM.__init__(self, card_number=self.card_number, pin_code=self.pin_code, balance=self.balance)
 
-    def __exit__(self):
+    def exit(self):
         print('Bye!')
+        exit()
 
 
-ATM()
+ATM.__init__(self, card_number=0, pin_code=0, balance=0)
