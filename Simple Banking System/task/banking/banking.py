e3fd8708b25c8355bd163f92a6c88061f50857c5
Benjamin A Pillmore <41969676+bapillmore@users.noreply.github.com>
2021-05-07 18:26:00 +0200

    ATM - Stage 2 Complete (Luhn)

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,9 +1,9 @@
 # Write your code here
 import random
+import math
 
 
 class ATM:
-
     card_number = None
     pin_code = None
     balance = None
@@ -21,8 +21,26 @@ class ATM:
 
     @staticmethod
     def generator():
-        base_card = '400000' + str(random.randrange(100000000, 999999999) + random.randrange(1, 9))
-        ATM.card_number = base_card
+        base_card = list(str('400000' + str(random.randrange(100000000, 999999999))))
+        numbers = dict(zip(list(range(1, 16)), base_card))
+        multipliers = dict(zip(list(range(1, 16)), '212121212121212'))
+        for values in numbers:
+            numbers[values] = int(numbers[values])
+        for values in multipliers:
+            multipliers[values] = int(multipliers[values])
+        new_numbers = {key: value * multipliers[key] for key, value in numbers.items() if key in multipliers}
+        for value in new_numbers:
+            if new_numbers[value] > 9:
+                new_numbers[value] -= 9
+        luhn_total = sum(new_numbers.values())
+
+        def round_up(n, decimals=-1):
+            multiplier = 10 ** decimals
+            return int(math.ceil(n * multiplier) / multiplier)
+
+        check_digit = round_up(luhn_total) - luhn_total
+        final_card_number = ''.join(base_card) + str(check_digit)
+        ATM.card_number = final_card_number
         ATM.pin_code = str(random.randrange(1000, 9999))
         ATM.balance = round(0, 2)
         print(f'Your card has been created \nYour card number: \n{ATM.card_number}\nYour card PIN: \n{ATM.pin_code}')
